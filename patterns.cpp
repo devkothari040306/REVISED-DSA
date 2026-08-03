1ff6169bd515f606b42de265e02234fe35097280
Dev <dkothari703@gmail.com>
2026-08-04 00:37:10 +0530

    Patterns are revised

--- patterns.cpp
+++ patterns.cpp
@@ -1,88 +1,112 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main () {
+//     // int n =4; // square pattern
+//     // for(int i = 1; i<=n; i++) {
+//     //     for(int j = 1; j<= n; j++) {
+//     //         cout<<"*";
+//     //     }
+//     //     cout<<endl;
+//     // }
+    
+//     // for(int i = 1; i<=n; i++) {    pattern triangle increasing
+//     //     for(int j = 1; j<= i; j++) {
+//     //         cout<<"*";
+//     //     }
+//     //     cout<<endl;
+//     // }
+
+// //     for(int i =1; i<=n; i++) {
+        
+// //         for(int j=1; j<=i; j++){
+// //    cout<<j<<" ";
+// //         }
+// //         cout<<endl;
+// //     }
+// //     for(int i =1; i<=n; i++) {
+        
+// //         for(int j=1; j<=i; j++){
+// //    cout<<i<<" ";
+// //         }
+// //         cout<<endl;
+// //     }
+// //     for(int i =1; i<=n; i++) {
+        
+// //         for(int j=i; j<=n; j++){
+// //    cout<<"*";
+// //         }
+// //         cout<<endl;
+// //     }
+// //  for(int i =1; i<=n; i++) {
+// //         int num =1;
+// //         for(int j=i; j<=n; j++){
+// //    cout<<num++;
+// //         }
+// //         cout<<endl;
+// //     }
+
+// // for(int i =1; i<=n; i++) {
+       
+// //         for(int j=1; j<=(n-i); j++){
+// //    cout<<" ";
+// //         }
+// //          for(int j=1; j<=i; j++){
+// //    cout<<"*";
+// //         }
+// //         for(int j =0; j<i-1; j++) {
+// //             cout<<"*";
+// //         }
+// //         cout<<endl;
+// //     }
+// // for(int i =1; i<=n; i++) { TWO PYRAMIDS
+       
+// //         for(int j=1; j<i; j++){
+// //    cout<<" ";
+// //         }
+// //          for(int j=i; j<=n; j++){
+// //    cout<<"*";
+// //         }
+// //         for(int j =n-i; j>=1; j--) {
+// //             cout<<"*";
+// //         }
+// //         cout<<endl;
+// //     }
+// int n = 5;
+// for(int i =1 ; i<=n; i++) {
+//     for(int j=1; j<=i; j++) {
+// cout<<"*";
+//     }   //
+//     cout<<endl;
+// }
+//     for(int i = 1; i<=n; i++) {
+//     for(int j=1;j<=n-i; j++) {
+//         cout<<"*";
+//     }
+//     cout<<endl;
+// }
+//     return 0;
+// }
 #include<bits/stdc++.h>
 using namespace std;
+// int main () {
+//     int n=5;
+//     for(int i = 0 ; i<n; i++) {
+//         for(int j=0; j< n; j++) {
+// cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+
+// }
 int main () {
-    // int n =4; // square pattern
-    // for(int i = 1; i<=n; i++) {
-    //     for(int j = 1; j<= n; j++) {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
-    
-    // for(int i = 1; i<=n; i++) {    pattern triangle increasing
-    //     for(int j = 1; j<= i; j++) {
-    //         cout<<"*";
-    //     }
-    //     cout<<endl;
-    // }
-
-//     for(int i =1; i<=n; i++) {
-        
-//         for(int j=1; j<=i; j++){
-//    cout<<j<<" ";
-//         }
-//         cout<<endl;
-//     }
-//     for(int i =1; i<=n; i++) {
-        
-//         for(int j=1; j<=i; j++){
-//    cout<<i<<" ";
-//         }
-//         cout<<endl;
-//     }
-//     for(int i =1; i<=n; i++) {
-        
-//         for(int j=i; j<=n; j++){
-//    cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-//  for(int i =1; i<=n; i++) {
-//         int num =1;
-//         for(int j=i; j<=n; j++){
-//    cout<<num++;
-//         }
-//         cout<<endl;
-//     }
-
-// for(int i =1; i<=n; i++) {
-       
-//         for(int j=1; j<=(n-i); j++){
-//    cout<<" ";
-//         }
-//          for(int j=1; j<=i; j++){
-//    cout<<"*";
-//         }
-//         for(int j =0; j<i-1; j++) {
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// for(int i =1; i<=n; i++) { TWO PYRAMIDS
-       
-//         for(int j=1; j<i; j++){
-//    cout<<" ";
-//         }
-//          for(int j=i; j<=n; j++){
-//    cout<<"*";
-//         }
-//         for(int j =n-i; j>=1; j--) {
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-int n = 5;
-for(int i =1 ; i<=n; i++) {
-    for(int j=1; j<=i; j++) {
-cout<<"*";
-    }   //
-    cout<<endl;
-}
-    for(int i = 1; i<=n; i++) {
-    for(int j=1;j<=n-i; j++) {
-        cout<<"*";
+    int n =4;
+      for(int i = 0 ; i<n; i++) {
+        for(int j=0; j<=i; j++) {
+cout<<"*"<<" ";
+        }
+        cout<<endl;
     }
-    cout<<endl;
-}
+    return 0;
     return 0;
 }
